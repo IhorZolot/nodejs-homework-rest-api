7dbcbe44f0ddaa6b48874d21f6ea5f4b33f09e79
Ihor <fedonn@ukr.net>
2023-10-24 01:06:53 +0300

    update lesson 3

--- routes/api/contacts-routes.js
+++ routes/api/contacts-routes.js
@@ -1,16 +1,24 @@
 import express from 'express'
-
 import contactsControllers from '../../controllers/contacts-controllers.js'
+import { validateBody } from '../../decorators/index.js'
+import { contactAddSchema, contactUpdateSchema, contactUpdateFavoriteSchema } from '../../models/Contact.js'
+import { isValidId } from '../../middlewares/index.js'
+
 const contactsRouter = express.Router()
 
 contactsRouter.get('/', contactsControllers.getAll)
 
-contactsRouter.get('/:id', contactsControllers.getById)
+contactsRouter.get('/:id', isValidId, contactsControllers.getById)
 
-contactsRouter.post('/',contactsControllers.add)
+contactsRouter.post('/', validateBody(contactAddSchema), contactsControllers.add)
 
-contactsRouter.put('/:id', contactsControllers.updateById)
-
-contactsRouter.delete('/:id', contactsControllers.deleteById)
+contactsRouter.put('/:id', isValidId, validateBody(contactUpdateSchema), contactsControllers.updateById)
+contactsRouter.patch(
+	'/:id/favorite',
+	isValidId,
+	validateBody(contactUpdateFavoriteSchema),
+	contactsControllers.updateFavorite
+),
+	contactsRouter.delete('/:id', isValidId, contactsControllers.deleteById)
 
 export default contactsRouter
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-import app from './app.js'
+import mongoose from 'mongoose'
 
-app.listen(3000, () => {
-	console.log('Server running. Use our API on port: 3000')
-})
+import app from './app.js'
+// aMqMWWXOCzzq1aWS
+const DB_HOST =
+	'mongodb+srv://Ihor:aMqMWWXOCzzq1aWS@cluster0.hzc4eni.mongodb.net/my-contacts?retryWrites=true&w=majority'
+
+mongoose
+	.connect(DB_HOST)
+	.then(() => {
+		console.log('Database connection successful')
+		app.listen(3000, () => {
+			console.log('Server running. Use our API on port: 3000')
+		})
+	})
+	.catch(error => {
+		console.error('Database connection error:', error.message)
+		process.exit(1)
+	})
