7f0d7c4903f141f66588980fa0496edc8c71b98b
Ihor <fedonn@ukr.net>
2023-11-07 23:05:59 +0200

    finaly task 6

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.static('public'))
 
 app.use('/api/auth', authRouter)
-app.use('/user', authRouter)
+app.use('/users', authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -4,16 +4,17 @@ import dotenv from 'dotenv'
 import gravatar from 'gravatar'
 import fs from 'fs/promises'
 import path from 'path'
-import User from '../models/User.js'
+import { nanoid } from 'nanoid'
 
-import { HttpError } from '../helpers/index.js'
+import User from '../models/User.js'
+import { HttpError, sendEmail } from '../helpers/index.js'
 import { ctrlWrapper } from '../decorators/index.js'
 
 const avatarPath = path.resolve('public', 'avatars')
 
 dotenv.config()
 
-const { JWT_SECRET } = process.env
+const { JWT_SECRET, BASE_URL } = process.env
 
 const signup = async (req, res) => {
 	const { email, password } = req.body
@@ -25,20 +26,65 @@ const signup = async (req, res) => {
 		return res.status(400).json({ error: 'All fields must be filled' })
 	}
 	const avatarURL = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' })
-
 	const hashPassword = await bcrypt.hash(password, 6)
-	const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL })
+	const verificationToken = nanoid()
+
+	const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationToken })
+const verifyEmail = {
+	to: email,
+	subject: 'Veryfy email',
+	html: `<a target='_blank' href='${BASE_URL}/api/auth/verify/${verificationToken}'>Click to verify email</a>`
+}
+await sendEmail(verifyEmail)
 	res.status(201).json({
 		username: newUser.username,
 		email: newUser.email,
 		avatarURL: newUser.avatarURL,
 	})
 }
+const resendVerifyEmail = async (req, res) => {
+	const {email} = req.body
+	const user = await User.findOne({email})
+	if(!user) {
+		throw HttpError(404, 'Email not Found')
+	}
+	if(user.verify) {
+		throw HttpError(400, 'Email already verify')
+	}
+	const verifyEmail = {
+		to: email,
+		subject: 'Veryfy email',
+		html: `<a target='_blank' href='${BASE_URL}/api/auth/verify/${user.verify}'>Click to verify email</a>`
+	}
+	await sendEmail(verifyEmail)
+
+	res.json({
+		message: 'Verify email send success'
+	})
+
+}
+
+const verify = async (req, res) => {
+	const {verificationToken} = req.params
+	const user = await User.findOne({verificationToken})
+  if(!user){
+		throw HttpError(404, 'Email not found')
+	}
+	await User.findByIdAndUpdate(user._id, {verify: true, verificationToken: ''})
+	res.json({
+		message: 'Email verify success'
+	})
+}
+
+
 const signin = async (req, res) => {
 	const { email, password } = req.body
 	const user = await User.findOne({ email })
 	if (!user) {
 		throw HttpError(401, 'Email or password invalid')
+	}
+	if(!user.verify){
+		throw HttpError (401, 'Email not veryfy')
 	}
 	const passwordCompare = await bcrypt.compare(password, user.password)
 	if (!passwordCompare) {
@@ -70,7 +116,7 @@ const updateAvatar = async (req, res) => {
 		const avatarURL = path.join('avatars', filename)
 		user.avatarURL = avatarURL
 		await user.save()
-		res.status(201).json(user)
+		res.status(200).json(user)
 	} catch (error) {
 		throw HttpError(401)
 	}
@@ -85,6 +131,8 @@ const signout = async (req, res) => {
 export default {
 	signup: ctrlWrapper(signup),
 	signin: ctrlWrapper(signin),
+	resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
+	verify: ctrlWrapper(verify),
 	signout: ctrlWrapper(signout),
 	getCurrent: ctrlWrapper(getCurrent),
 	updateAvatar: ctrlWrapper(updateAvatar),
